9673308bcc3d348b96b9e81c1334154eee2e2229
YOVO Amèvi <107300417+Amescovitch@users.noreply.github.com>
2023-04-24 22:37:46 +0000

    Update settings.py

--- FirstFight/settings.py
+++ FirstFight/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'FirstFight.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,6 +94,7 @@ DATABASES = {
         'PORT': '6278',
     }
 }
+'''
 
 
 # Password validation
